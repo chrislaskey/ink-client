2c9a803aaafe82cb4d556f367b35c42fda58d55b
Chris Laskey <contact@chrislaskey.com>
2017-07-24 15:21:41 -0400

    Add antd library

--- src/pages/Posts/All.js
+++ src/pages/Posts/All.js
@@ -4,6 +4,7 @@ import { map } from 'lodash'
 import { Link } from 'react-router-dom'
 import { getPosts } from '../../api/posts'
 import Page from '../../components/Page'
+import { Button } from 'antd'
 
 export const AllPosts = ({data: {loading, posts}, match}) => {
   const renderPosts = (items) => (
@@ -17,6 +18,7 @@ export const AllPosts = ({data: {loading, posts}, match}) => {
   return (
     <Page loading={loading}>
       <h1>Posts</h1>
+      <Button type="primary">Hello Button</Button>
       <p>
         <Link to='/posts/new'>New Post</Link>
       </p>
