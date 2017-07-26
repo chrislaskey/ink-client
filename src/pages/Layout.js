f4c56649ca196cdb4a1ae33c9e001995277263f4
Chris Laskey <contact@chrislaskey.com>
2017-07-26 10:01:00 -0400

    Work around React Router v4 fundamental design error

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Route, withRouter } from 'react-router-dom'
+import { isLoggedIn } from '../reducers/currentUser'
 import { Layout as LayoutComponent } from 'antd'
 import Sidebar from './_Sidebar'
 import './Layout.css'
 
 import Home from './Home/Index'
+import Login from './Login'
 import Posts from './Posts'
 import Settings from './Settings'
 
-const Layout = () => (
-  <LayoutComponent className='ant-layout-has-sider window-height'>
-    <Sidebar />
-    <Route exact path='/' component={Home} />
-    <Route path='/posts' component={Posts} />
-    <Route path='/settings' component={Settings} />
-  </LayoutComponent>
-)
+const Layout = ({ loggedIn }) => {
+  if (!loggedIn) {
+    return <Login />
+  }
 
-export default Layout
+  return (
+    <LayoutComponent className='ant-layout-has-sider window-height'>
+      <Sidebar />
+      <Route exact path='/' component={Home} />
+      <Route path='/posts' component={Posts} />
+      <Route path='/settings' component={Settings} />
+    </LayoutComponent>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  loggedIn: isLoggedIn(state)
+})
+
+export default withRouter(connect(mapStateToProps)(Layout))
