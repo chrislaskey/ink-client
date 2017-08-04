d00731100a1d7a9b68ba663e4c9a992cbbd8ff34
Chris Laskey <contact@chrislaskey.com>
2017-08-04 18:08:27 -0400

    Show labels on post

--- src/pages/Posts/_Post.js
+++ src/pages/Posts/_Post.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { isEmpty, map } from 'lodash'
 import Markdown from '../../components/Markdown'
 
 export const Post = ({ onCheck, post }) => (
   <div className='post'>
     <div className='post-header'>
       <h1>{post.title}</h1>
+      { !isEmpty(post.labels) &&
+        <p>
+          <strong>Labels: </strong>
+          {map(post.labels, (label) => (
+            <span key={label.id}>{label.name}</span>
+          ))}
+        </p>
+      }
     </div>
     <div className='post-body'>
       <Markdown onCheck={onCheck} value={post.body} />
