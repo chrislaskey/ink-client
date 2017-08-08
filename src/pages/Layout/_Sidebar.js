c7374a4381ac0087b685e3683372cefd5af1876d
Chris Laskey <contact@chrislaskey.com>
2017-08-08 18:19:26 -0400

    Dynamically render labels in sidebar menu

--- src/pages/Layout/_Sidebar.js
+++ src/pages/Layout/_Sidebar.js
@@ -1,61 +1,73 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { graphql } from 'react-apollo'
+import { map } from 'lodash'
+import { getLabels } from '../../api/labels'
 import { Link } from 'react-router-dom'
 import { Icon, Layout, Menu } from 'antd'
 import { updatePreferences } from '../../actions/currentUser'
 import { getPreference } from '../../reducers/currentUser'
 
-export const Sidebar = ({ sidebarCollapsed, toggleSidebar }) => (
-  <Layout.Sider
-    id='sidebar'
-    className='window-height'
-    collapsible
-    collapsed={sidebarCollapsed}
-    onCollapse={(collapsed) => toggleSidebar(collapsed)}
-    width='240'
-  >
-    <div className='section-heading' />
-    <div className='scroll-container'>
-      <Menu
-        id='main-nav'
-        mode='inline'
-        defaultSelectedKeys={['notes2']}
-        defaultOpenKeys={['notes2']}
-      >
-        <Menu.Item key='0'>
-          <Link to='/'>
-            <Icon type='home' />
-            <span className='nav-text'>
-              Home
-            </span>
-          </Link>
-        </Menu.Item>
-        <Menu.SubMenu
-          key='notes'
-          title={<Link to='/notes'><Icon type='mail'/><span className='nav-text'> Notes</span></Link>}
+export const Sidebar = ({ data: { labels }, sidebarCollapsed, toggleSidebar }) => {
+  const renderLabel = (label) => (
+    <Menu.Item key={'label-' + label.id}>
+      {label.name}
+    </Menu.Item>
+  )
+
+  return (
+    <Layout.Sider
+      id='sidebar'
+      className='window-height'
+      collapsible
+      collapsed={sidebarCollapsed}
+      onCollapse={(collapsed) => toggleSidebar(collapsed)}
+      width='240'
+    >
+      <div className='section-heading' />
+      <div className='scroll-container'>
+        <Menu
+          id='main-nav'
+          mode='inline'
+          defaultSelectedKeys={['notes2']}
+          defaultOpenKeys={['notes2']}
         >
-          <Menu.Item key='1'>
-            <Link to='/notes'>All</Link>
+          <Menu.Item key='0'>
+            <Link to='/'>
+              <Icon type='home' />
+              <span className='nav-text'>
+                Home
+              </span>
+            </Link>
           </Menu.Item>
-          <Menu.ItemGroup key='labels' title='Labels'>
-            <Menu.Item key='one'>One</Menu.Item>
-            <Menu.Item key='two'>Two</Menu.Item>
-          </Menu.ItemGroup>
-        </Menu.SubMenu>
-        <Menu.Item key='settings'>
-          <Link to='/settings'>
-            <Icon type='setting' />
-            <span className='nav-text'>
-              Settings
-            </span>
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </div>
-  </Layout.Sider>
-)
+          <Menu.SubMenu
+            key='notes'
+            title={<Link to='/notes'><Icon type='mail' /><span className='nav-text'> Notes</span></Link>}
+          >
+            <Menu.Item key='1'>
+              <Link to='/notes'>All</Link>
+            </Menu.Item>
+            <Menu.ItemGroup key='labels' title='Labels'>
+              {map(labels, renderLabel)}
+            </Menu.ItemGroup>
+          </Menu.SubMenu>
+          <Menu.Item key='settings'>
+            <Link to='/settings'>
+              <Icon type='setting' />
+              <span className='nav-text'>
+                Settings
+              </span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+    </Layout.Sider>
+  )
+}
 
 Sidebar.displayName = 'Sidebar'
+
+const SidebarWithData = graphql(getLabels)(Sidebar)
 
 const mapStateToProps = (state) => {
   const sidebarDefault = window.innerWidth <= 1000
@@ -73,4 +85,4 @@ const mapDispatchToProps = (dispatch) => ({
   )
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarWithData)
