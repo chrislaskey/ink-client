5dc91b9a9591f5eee925ea3b8ea3da4522db2dbe
Chris Laskey <contact@chrislaskey.com>
2018-07-06 16:34:27 -0400

    Update Enzyme

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,5 +1,9 @@
 'use strict';
 
+global.requestAnimationFrame = (callback) => {
+  setTimeout(callback, 0)
+}
+
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
   // inconsistent state due to an error, but it gets swallowed by a Promise,
