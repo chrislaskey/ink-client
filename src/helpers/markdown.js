f69345bcdb447939394708693fe5e013eee3059b
Chris Laskey <contact@chrislaskey.com>
2018-07-07 18:57:19 -0400

    Whitelist checkbox inputs

--- src/helpers/markdown.js
+++ src/helpers/markdown.js
@@ -32,11 +32,12 @@ export const markdownToHtml = (value, options = {}) => {
       a: [ 'href', 'name', 'target' ],
       code: [ 'class' ],
       img: [ 'src' ],
+      input: [ 'checked', 'disabled', 'style', 'type' ],
       span: [ 'class' ]
     },
     allowedTags: [
       'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-      'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+      'nl', 'li', 'b', 'i', 'input', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
       'span', 'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'
     ]
   })
